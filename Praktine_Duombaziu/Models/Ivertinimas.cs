0f41cbdfa85dda1c664442384ba099d3697a8c66
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-05-25 18:59:11 +0300

    Added ivertinimas
    dwdwd

--- Praktine_Duombaziu/Models/Ivertinimas.cs
+++ Praktine_Duombaziu/Models/Ivertinimas.cs
@@ -8,5 +8,21 @@ namespace Praktine_Duombaziu.Models
 {
     public class Ivertinimas
     {
+        public int IvertinimoID { get; set; }
+        public int KurjerioIvertinimas { get; set; }
+        public int ProduktoIvertinimas { get; set; }
+        public string Komentaras { get; set; }
+        public int KlientoID { get; set; }
+        public int UzsakymoID { get; set; }
+
+        public Ivertinimas(int ivertinimoID, int kurjerioIvertinimas, int produktoIvertinimas, string komentaras, int klientoID, int uzsakymoID)
+        {
+            IvertinimoID = ivertinimoID;
+            KurjerioIvertinimas = kurjerioIvertinimas;
+            ProduktoIvertinimas = produktoIvertinimas;
+            Komentaras = komentaras;
+            KlientoID = klientoID;
+            UzsakymoID = uzsakymoID;
+        }
     }
 }
