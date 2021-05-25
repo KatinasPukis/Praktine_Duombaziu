83ad0feec0046f65ef1cba1dd7d9d70354251a0f
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-05-25 19:02:37 +0300

    blet klientas

--- Praktine_Duombaziu/Models/Klientas.cs
+++ Praktine_Duombaziu/Models/Klientas.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Praktine_Duombaziu.Models
 {
-    class Klientas
+   public class Klientas
     {
+        public int KlientoID { get; set; }
+        public string Vardas { get; set; }
+        public string Pavarde { get; set; }
+
+        public string ElPastas { get; set; }
+        public string TelNr { get; set; }
+        public DateTime Gimtadienis { get; set; }
+
+        public Klientas(int klientoID, string vardas, string pavarde, string elPastas, string telNr, DateTime gimtadienis)
+        {
+            KlientoID = klientoID;
+            Vardas = vardas;
+            Pavarde = pavarde;
+            ElPastas = elPastas;
+            TelNr = telNr;
+            Gimtadienis = gimtadienis;
+        }
     }
+
 }
