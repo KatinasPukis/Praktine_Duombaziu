2944d700778eb7af147706779e9ed6344923faca
Dainoras Žiukas <65944827+dainoras99@users.noreply.github.com>
2021-05-25 18:57:22 +0300

    2 models added
    added uzsakymoPreke and TechninePagalba model classes

---
+++ Praktine_Duombaziu/Models/TechninePagalba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktine_Duombaziu.Models
+{
+    public class TechninePagalba
+    {
+        public int id { get; set; }
+        public int transportoId { get; set; }
+        public int senoKurjerioId { get; set; }
+        public int naujoKurjerioId { get; set; }
+        public int uzsakymoId { get; set; }
+
+        public TechninePagalba(int id, int transportoId, int senoKurjerioId, int naujoKurjerioId, int uzsakymoId)
+        {
+            this.id = id;
+            this.transportoId = transportoId;
+            this.senoKurjerioId = senoKurjerioId;
+            this.naujoKurjerioId = naujoKurjerioId;
+            this.uzsakymoId = uzsakymoId;
+        }
+    }
+}
---
+++ Praktine_Duombaziu/Models/UzsakymoPreke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktine_Duombaziu.Models
+{
+    public class UzsakymoPreke
+    {
+        public int operacijosNr { get; set; }
+        public int prekesId { get; set; }
+        public int kiekis { get; set; }
+        public double kaina { get; set; }
+        public int partImonesMeniuId { get; set; }
+        public int uzsakymoId { get; set; }
+
+        public UzsakymoPreke(int operacijosNr, int prekesId, int kiekis, double kaina, int partImonesMeniuId, int uzsakymoId)
+        {
+            this.operacijosNr = operacijosNr;
+            this.prekesId = prekesId;
+            this.kiekis = kiekis;
+            this.kaina = kaina;
+            this.partImonesMeniuId = partImonesMeniuId;
+            this.uzsakymoId = uzsakymoId;
+        }
+    }
+}
